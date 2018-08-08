a28288bd3d98e380155aec9cb6fa8d4db0d92c7b
zoe <z.zoelec2@gmail.com>
2018-08-08 14:57:07 -0700

    Initial Commit

---
+++ icons8/icons8.h
@@ -0,0 +1,19 @@
+//
+//  icons8.h
+//  icons8
+//
+//  Created by Zoe IAMZOE.io on 8/8/18.
+//  Copyright © 2018 Zoe IAMZOE.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for icons8.
+FOUNDATION_EXPORT double icons8VersionNumber;
+
+//! Project version string for icons8.
+FOUNDATION_EXPORT const unsigned char icons8VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <icons8/PublicHeader.h>
+
+
