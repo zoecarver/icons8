a25f7ed4b98047fa91e2a28ce94a68c740057439
zoe <z.zoelec2@gmail.com>
2018-08-23 11:09:10 -0700

    fix: bundle id for mac

--- icons8-mac/Icons.swift
+++ icons8-mac/Icons.swift
@@ -9,6 +9,6 @@
 import AppKit
 
 public func icon(named name: icons8) -> NSImage {
-    let bundle = Bundle(identifier: "com.zoecarver.icons8")
+    let bundle = Bundle(identifier: "com.zoecarver.icons8-mac")
     return bundle?.image(forResource: NSImage.Name(name.rawValue)) ?? NSImage()
 }
